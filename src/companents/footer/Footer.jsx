4864b166a0623b330cd8df22c54de86dec9c7119
kifatet-n <n.kifayet17@gmail.com>
2024-07-14 22:52:40 +0400

    create footer

--- src/companents/footer/Footer.jsx
+++ src/companents/footer/Footer.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='footer flex px-[120px] py-[80px]'>
-        <div className='footer-left'>
-        <h1 className='font-size-20px'>Concat</h1>
-        <p>Feel free to reach out!</p>
+    <footer className='footer  bg-slate-900 flex justify-between px-[100px] py-[60px]'>
+        <div className='footer-left py-3'>
+        <h1 className='footer-title text-5xl text-white'>Concat</h1>
+        <p className='text-white text-2xl'>Feel free to reach out!</p>
         </div>
-        <div className='footer-right flex justify-between'>
-            <div>
-            <img src="./src/assets/contact/eamilIcon.png"  />
-            <p> myemail@email.com</p>
+        <div className='footer-right columns-1'>
+            <div className='flex gap-2'>
+            <img src="./src/assets/contact/emailIcon.png" />
+            <p className='text-xl text-white py-3'>myemail@email.com</p>
             </div>
-            <div>
-            <img src="./src/assets/contact/githubIcon.png"/>
-            <p>linkedin.com/myname </p>
+            <div className='flex gap-2'>
+            <img src="./src/assets/contact/linkedinIcon.png"/>
+            <p className='text-xl text-white py-3'>linkedin.com/myname </p>
             </div>
-            <div>
-            <img src="./src/assets/contact/linkedinIcon.png" />
-            <p>github.com/myname </p>
+            <div className='flex gap-2'>
+            <img src="./src/assets/contact/githubIcon.png" />
+            <p className='text-xl text-white py-3'>github.com/myname </p>
             </div>
 
         </div>
